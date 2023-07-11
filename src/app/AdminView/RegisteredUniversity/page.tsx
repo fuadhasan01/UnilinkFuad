55acca10abb5997a5e344cbe23207937deead87b
Fuad Hasan Labib <labib.knc@gmail.com>
2023-07-12 00:56:52 +0600

    page header created

--- src/app/AdminView/RegisteredUniversity/page.tsx
+++ src/app/AdminView/RegisteredUniversity/page.tsx
@@ -3,8 +3,16 @@
 
 export default function Home() {
     return (
-        <div>
-            <h1 className="text-4xl text-center mt-10 font-bold">New Registered University</h1>
+        <div className="flex w-9/12 m-auto justify-between">
+            <div>
+                <h1 className="text-3xl mt-10    bg-gray-600 px-11 py-3 text-white rounded-lg">Registered University</h1>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                <h1 className="text-3xl mt-10 bg-gray-600 px-11 py-3 text-white rounded-lg">Recently Added</h1>
+            </div>
 
             
         </div>
